3bdbed2cd96608193ecc1020b659e0bec57a4d37
Sami Hangaslammi <shang@iki.fi>
2011-11-30 21:28:07 +0200

    bugfix: delayEn retained all old events

--- src/Control/Coroutine/FRP.hs
+++ src/Control/Coroutine/FRP.hs
@@ -78,8 +78,9 @@ delayE delay = arr (const delay) &&& C.id >>> delayEn
 
 delayEn :: Coroutine (Int, Event e) (Event e)
 delayEn = Coroutine $ step 0 IntMap.empty where
-    step !cur !buffer (delay, ev) = (ev', Coroutine $ step (cur+1) buffer') where
+    step !cur !buffer (delay, ev) = (ev', Coroutine $ step (cur+1) buffer'') where
         ev' = IntMap.findWithDefault [] cur buffer'
         buffer'
             | null ev   = buffer
             | otherwise = IntMap.insertWith (++) (cur+delay) ev buffer
+        buffer'' = IntMap.delete cur buffer'
